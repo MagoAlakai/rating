c8d6c855d748a726f77b6360c3dd3838b5d31989
Jose Maria Farfan <magoalakai@gmail.com>
2020-10-27 17:17:35 +0100

    Fase 4

---
+++ Fase 4/main.js
@@ -0,0 +1,11 @@
+var change_color = (event) =>{
+    var corazones = document.getElementsByClassName('far');
+    var arr = Array.prototype.slice.call(corazones)
+    var index = arr.indexOf(event);
+    console.log(corazones);
+    console.log(index);
+    for(let i = 0; i<= index; i++){
+        arr[i].style.color='blue';
+    }
+}
+
