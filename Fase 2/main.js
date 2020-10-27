1e1621c9e4d1200420f04290a010ecf91aea963e
Jose Maria Farfan <magoalakai@gmail.com>
2020-10-27 17:11:24 +0100

    Fase 3

---
+++ Fase 2/main.js
@@ -0,0 +1,7 @@
+var change_color = (event) =>{
+    event.style.color='blue';
+}
+
+var change_color_back = (event) =>{
+    event.style.color='red';
+}
---
+++ Fase 3/main.js
@@ -0,0 +1,24 @@
+var change_color = (event) =>{
+    var corazones = document.getElementsByClassName('far');
+    var arr = Array.prototype.slice.call(corazones)
+    var index = arr.indexOf(event);
+    console.log(corazones);
+    console.log(index);
+    for(let i = 0; i<= index; i++){
+        arr[i].style.color='blue';
+    }
+}
+var change_color_back = (event) =>{
+    var corazones = document.getElementsByClassName('far');
+    var arr = Array.prototype.slice.call(corazones)
+    var index = arr.indexOf(event);
+    console.log(corazones);
+    console.log(index);
+    for(let i = 0; i<= index; i++){
+        arr[i].style.color='red';
+    }
+}
+
+/*while(corazones !== event){
+    event.style.color='blue';
+}*/
