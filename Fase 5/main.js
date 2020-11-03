3acfbe0374a4b4e5f9f13c0218bcc5c963f748aa
Jose Maria Farfan <magoalakai@gmail.com>
2020-11-03 11:51:38 +0100

    Solución Final

--- Fase 5/main.js
+++ Fase 5/main.js
@@ -14,15 +14,15 @@ var change_color = (event) =>{
     var corazones = document.getElementsByClassName('far');
     var arr = Array.prototype.slice.call(corazones)
     var index = arr.indexOf(event);
-    for(let i = 0; i<= index; i++){
+    for(let i = index; i>= index; i++){
         if(arr[i].className === 'red far fa-heart active'){
             i = index;
-            for(let i = 0; i < corazones.length; i++){
+            for(let i = index; i>= index; i++){
                 arr[i].classList.remove('active');
                 }
         }else{
             arr[i].classList.add('active');
-        } 
+        }
     }
 }
 
